0ba3a6d189e4a7fd036cc3ba45ba853f51631e1e
jaroinside <jaro0116@gmail.com>
2019-02-07 15:44:13 +0900

    Ch2. JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const text = '당신은 어썸한가요?';
+    const condition = true;
+    const style = {
+      backgroundColor: 'gray',
+      border: '1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      WebkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="my-div">
+        <h1>리액트 안녕!</h1>
+        <h2>{text}</h2>
+        {
+          condition ? '참' : '거짓'
+        }
+        <hr />
+        {
+          condition && '보여주세요'
+        }
+        <div
+          style={style}
+          // 여기는 주석이 가능
+          /**
+           * 주석이 가능하다
+           */
+        />
+        // 여기는 주석 안됨. 그대로 렌더링
+        /**
+         * 역시 그대로 렌더링
+         */
+        { /* 여기서는 이렇게 주석을 사용한다. */ }
       </div>
     );
   }
